729cacca3ca39379414d13209d3770c89f39bbe1
Benedict Nderitu <giteebenedict@gmail.com>
2025-07-01 09:35:48 +0300

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,68 @@
+-- performance.sql
+
+SELECT 
+    b.booking_id,
+    b.booking_date,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.user_id
+JOIN 
+    properties p ON b.property_id = p.property_id
+LEFT JOIN 
+    payments pay ON b.booking_id = pay.booking_id;
+
+
+EXPLAIN 
+SELECT 
+    b.booking_id,
+    b.booking_date,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.user_id
+JOIN 
+    properties p ON b.property_id = p.property_id
+LEFT JOIN 
+    payments pay ON b.booking_id = pay.booking_id;
+
+
+-- Optimized version
+
+SELECT 
+    b.booking_id,
+    b.booking_date,
+    u.first_name,
+    u.last_name,
+    p.property_name,
+    p.location,
+    pay.amount,
+    pay.payment_date
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.user_id
+JOIN 
+    properties p ON b.property_id = p.property_id
+LEFT JOIN 
+    payments pay ON b.booking_id = pay.booking_id;
