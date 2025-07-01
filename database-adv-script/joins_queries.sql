00a2d30aca21331257a747425d109c3732ada451
Benedict Nderitu <giteebenedict@gmail.com>
2025-07-01 09:06:48 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,40 @@
+INNER JOIN
+SELECT 
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date,
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    users.email
+FROM 
+    bookings
+INNER JOIN 
+    users ON bookings.user_id = users.user_id;
+
+LEFT JOIN
+SELECT 
+    properties.property_id,
+    properties.property_name,
+    properties.location,
+    reviews.review_id,
+    reviews.rating,
+    reviews.comment
+FROM 
+    properties
+LEFT JOIN 
+    reviews ON properties.property_id = reviews.property_id;
+
+FULL OUTER JOIN 
+SELECT 
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings ON users.user_id = bookings.user_id;
+
